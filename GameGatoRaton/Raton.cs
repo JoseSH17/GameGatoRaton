342641983673696800fc772c12b515416138f0e0
JoseSH17 <josesh17@gmail.com>
2021-10-29 22:53:59 -0600

    Increasing Random generation to allow better criteria to define the Cat player

---
+++ GameGatoRaton/Raton.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+
+namespace GameGatoRaton
+{
+    public class Raton : Jugador
+    {
+        private string Nombre;
+        private int Posicion = 30;
+        private int[] NumerosDisponibles = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        public override void SetNombre(string Nombre) { this.Nombre = Nombre; }
+        public override string GetNombre() { return Nombre; }
+        public override void ActualizarPosicion(int n) { Posicion -= n; }
+        public override int ObtenerPosicion() { return Posicion; }
+        public override bool ProximaPosicionValida(int n)
+        {
+            if (ObtenerPosicion() - n >= 30) return false;
+            return true;
+        }                
+        public override void ActualizarNumerosDisponibles(int n) => NumerosDisponibles = NumerosDisponibles.Where(v => v != n).ToArray();
+        public override void ObtenerNumerosDisponibles() => Console.WriteLine("Numeros disponibles: [{0}]", string.Join(",", NumerosDisponibles));
+        public override bool ValidarNumeroDisponible(int i)
+        {
+            var exists = NumerosDisponibles.Any(n => n == i);
+            if (exists) return true;
+            return false;
+        }
+        public override bool PoseeNumerosDisponibles() => NumerosDisponibles.Length != 0;
+
+        public override bool HayNumerosValidos()
+        {
+            bool t = false;
+            for (int i = 0; i < NumerosDisponibles.Length; i++)
+            {
+                if (ObtenerPosicion() - NumerosDisponibles[i] < 30) t = true;
+            }
+            return t;
+        }
+    }
+}
